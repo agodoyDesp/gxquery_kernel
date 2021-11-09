4ef10db8f555ea67b92cbfa2c3bff81394d1b0ee
agodoy <agodoy83@gmail.com>
2021-11-08 21:34:29 -0300

    se cambia para que muestre html

--- kernel/gxQueryKernel.py
+++ kernel/gxQueryKernel.py
@@ -10,17 +10,23 @@ class GxQueryKernel(Kernel):
     language_version = '0.1'
     language_info = {
         'name': 'gxQuery',
-        'mimetype': 'text/plain',
-        'file_extension': '.txt',
+        'mimetype': 'text/html',
+        'file_extension': '.html',
     }
     banner = "Rest test kernel "
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
         response = requests.get('http://localhost:5000/' + code)
-        stream_content = {'name': 'stdout', 'text': response.text}
-        print(stream_content)
-        self.send_response(self.iopub_socket,
-                           'stream', stream_content)
+        # stream_content = {'name': 'stdout', 'text': response.text}
+        # self.send_response(self.iopub_socket,
+        #                   'stream', stream_content)
+
+        print(response.text)
+        self.send_response(self.iopub_socket, 'display_data', {
+            'metadata': {},
+            'data': {
+                'text/html': response.text}
+        })
 
         return {'status': 'ok',
                 'execution_count':
