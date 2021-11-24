d9287f22b1b0fa91a6879a85b6dc9f8b942b2808
agodoy <agodoy83@gmail.com>
2021-11-24 19:14:51 -0300

    se agregan metodos start_session y set_metada

--- kernel/gxQueryKernel.py
+++ kernel/gxQueryKernel.py
@@ -1,6 +1,25 @@
 import requests
+import json
 
 from ipykernel.kernelbase import Kernel
+
+
+def start_session():
+    start_session_data = {'RepositoryName': '', 'UserName': 'andres', 'Password': 'andres'}
+    start_session_data_resp = requests.post('http://localhost:80/GXquery40/rest/GXquery_StartSessionService',
+                                            json=start_session_data)
+    resp = json.loads(start_session_data_resp.text)
+
+    return resp
+
+
+def set_metadata(session_token):
+    # Esta dando error el request
+    set_metadata_dict = {'CurrentMetaName': 'TravelAgencyGX16'}
+    set_metadata_headers = {'GeneXus-Agent': 'SmartDevice',
+                            'Authorization': 'OAuth' + session_token}
+    set_metadata_resp = requests.post('http://localhost:80/GXquery40/rest/GXquery_SetMetadataService',
+                                      json=set_metadata_dict, headers=set_metadata_headers)
 
 
 class GxQueryKernel(Kernel):
@@ -21,12 +40,14 @@ class GxQueryKernel(Kernel):
         # self.send_response(self.iopub_socket,
         #                   'stream', stream_content)
 
-        print(response.text)
         self.send_response(self.iopub_socket, 'display_data', {
             'metadata': {},
             'data': {
                 'text/html': response.text}
         })
+
+        start_session_data_resp = start_session()
+        set_metadata(start_session_data_resp['GXquerySessionToken'])
 
         return {'status': 'ok',
                 'execution_count':
